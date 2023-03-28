54e5d07b7fa1ed12b46381c06469952410428aee
Florian ESNAULT <florian61340@hotmail.fr>
2023-03-28 10:21:25 +0200

    Bulbasaur appears

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PokemonCard from '../components'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <><div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +30,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+    <div><PokemonCard />
+    </div></>
+  );
 }
 
 export default App
